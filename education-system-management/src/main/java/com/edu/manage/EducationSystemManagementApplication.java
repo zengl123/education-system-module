9ca1d98db12297432ef6cdf8aebae635628aa8b8
zengling <17363645521@163.com>
2019-09-27 21:20:02 +0800

    Initial commit

---
+++ education-system-management/src/main/java/com/edu/manage/EducationSystemManagementApplication.java
@@ -0,0 +1,28 @@
+package com.edu.manage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+/** 
+ * 文件名称: EducationSystemManagementApplication 
+ * 内容摘要:
+ * @author: zengling 
+ * 联系方式:zengling17925@talkweb.com.cn|17363645521
+ * @Date: 2019/9/27 12:52
+ * @version:  v1.0
+ * 
+ * 修改历史:  
+ * 修改日期       修改人员   版本	   修改内容  
+ * ----------------------------------------------
+ * 2019/9/27 12:52    zengling     v1.0    v1.0 XXXX
+ * 
+ * 版权:   版权所有(C)2012
+ * 公司:   拓维信息系统股份有限公司
+ */
+@SpringBootApplication
+public class EducationSystemManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationSystemManagementApplication.class, args);
+    }
+
+}
---
+++ education-system-portal/src/main/java/com/edu/portal/EducationSystemPortalApplication.java
@@ -0,0 +1,13 @@
+package com.edu.portal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EducationSystemPortalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EducationSystemPortalApplication.class, args);
+    }
+
+}
